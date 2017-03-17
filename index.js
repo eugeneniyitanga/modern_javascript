5d2247f771c4c0d1493aed92dc708fc951b6f752
Eugene Niyitanga <eugene.niyitanga2015@gmail.com>
2017-03-17 15:09:45 +0200

    added npm to run the server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Welcome to modern JavaScripts")
+
+var moment = require ("moment");
+
+var datetime = moment().startOf('day').fromNow();
+console.log(datetime);
+
+function test(x = 32) {
+  console.log(x);
+}
+test();
+
+console.log("Because I'm happy!");
+
